6912c2f67ec5b7661883a4d9313fb9bbe8a41350
chef <chef@recohut.com>
2021-08-30 17:45:09 +0000

    commit

---
+++ tests/test_logger.py
@@ -0,0 +1,25 @@
+import unittest
+import tempfile
+
+from src.logger import Logger
+
+
+class TestLogger(unittest.TestCase):
+    def setUp(self):
+        self.logger_name = tempfile.NamedTemporaryFile(suffix='.log').name
+        self.logger = Logger(self.logger_name)
+        self.logger.info('Unittest Message 1')
+        self.logger.warning('Unittest Message 2')
+        with open(self.logger_name, 'r') as l:
+            self.msg = l.readlines()
+
+    def testLogFileCreated(self):
+        self.assertTrue(os.path.exists(self.logger_name))
+
+    def testLogsWritten(self):
+        self.assertIn('Unittest Message 1',self.msg[0])
+        self.assertIn('Unittest Message 2',self.msg[1])
+    
+    def testLogTypes(self):
+        self.assertIn('INFO:',self.msg[0])
+        self.assertIn('WARNING:',self.msg[1])
